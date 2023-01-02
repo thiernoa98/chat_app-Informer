e6909ad5a31d47643ce8c83975fdfe7e58b8bb7d
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2023-01-02 01:03:39 -0500

    Add files via upload

---
+++ client/src/components/imports.js
@@ -0,0 +1,11 @@
+export {default as ChannelListContainer} from './ChannelListContainer';
+export {default as ChannelContainer} from './ChannelContainer';
+export {default as ChannelSearch} from './ChannelSearch';
+export {default as TeamChannelList} from './TeamChannelList';
+export {default as TeamChannelPreview} from './TeamChannelPreview';
+export {default as Authentic} from './Authentic';
+export {default as ChannelInner} from './ChannelInner';
+export {default as CreateChannel} from './CreateChannel';
+export {default as EditChannel} from './EditChannel';
+export {default as UserList} from './UserList';
+export {default as OutcomeDropdown} from './OutcomeDropdown';
