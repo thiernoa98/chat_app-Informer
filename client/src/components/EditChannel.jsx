710ffd9b28878e05a33345ab9efe7f055d23399d
thiernoa98 <117252418+thiernoa98@users.noreply.github.com>
2023-01-13 18:31:07 -0500

    Add files via upload

---
+++ client/src/components/EditChannel.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import {useChatContext} from 'stream-chat-react';
+import {UserList} from './imports';
+import { CloseCreateChannel } from '../assets';
+
+
+//input name for edited channel/group
+const ChannelNameInput = ({ channelName = channelName,setChannelName}) =>{
+
+  const handelChange = (event)=>{
+    event.preventDefault();
+
+    setChannelName(event.target.value);
+  }
+
+
+  return (
+    <div className='channel-name-input__wrapper'>
+      <p>Group Name: </p>
+      <input value={channelName} onChange={handelChange} placeholder="(no spaces)"/>
+      <p>Add people</p>
+    </div>
+  )
+}
+
+
+
+
+const EditChannel = ({setIsEditing}) => {
+
+  //getting the channel/group from the set using the chatContext var from stream-chat-react
+  const {channel} = useChatContext();
+ 
+  //the current group name
+  const [channelName, setChannelName] = useState(channel?.data?.name || channel?.data?.id );
+  const [addedUsers, setAddedUsers] = useState([]);
+
+
+
+  //action button func
+  const saveChanges = async(event) =>{
+    event.preventDefault();
+
+    //check if user has changed group name 
+    const nameChange = channelName !== (channel.data.name || channel.data.id);
+
+    //const user = addedUsers !== (channel.data.name || channel.data.id);
+
+    if(nameChange){
+      //{name:} we want to update the current name channelName (new)
+      await channel.update({name: channelName},{text: `Group Name has been changed to ${channelName}`});
+    }
+
+    //if name didn't change, but # users changes?
+    if(addedUsers.length ){
+      
+      await channel.addMembers(addedUsers) ;
+       
+    }
+    //all done, reset it
+    setChannelName(null);
+    setIsEditing(false);
+    setAddedUsers([]); 
+  }
+
+  return (
+    <div className='edit-channel__container'>
+      <div className='edit-channel__header'>
+
+        <p>Edit Group Name</p>
+
+        <CloseCreateChannel setIsEditing={setIsEditing} />
+
+      </div>
+      <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
+      <UserList addedUsers={addedUsers} setAddedUsers={setAddedUsers}/>
+
+      {/*button to save changes*/}
+      <div className='edit-channel__button-wrapper' onClick={saveChanges}>
+        <p>Save Edit</p>
+      </div>
+    </div>
+  )
+}
+
+export default EditChannel;
